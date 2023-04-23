afc947b709e74f8cf2f0a2f5b8969da55c627722
StevenMODIMO <stevepetes26@gmail.com>
2023-04-23 09:21:56 +0300

    ecomm:Ecommerce

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,20 @@
+import { useState , useEffect}  from "react"
+
 function App() {
+const [data, setData] = useState([])
+
+useEffect(() => {
+  const get = async() => {
+    const response = await fetch("http://localhost:5000/")
+    const data  = await response.json()
+    setData(data)
+  }
+  get()
+})
   return (
-    <div className="text-center text-4xl underline">
-     <h1>Hello</h1>
+    <div>
+     <h1 className="text-center text-4xl underline">Hello</h1>
+     <h2 className="text-9xl text-center m-10 text-green-500 font-mono">{data.json}</h2>
     </div>
   )
 }
--- server/server.js
+++ server/server.js
@@ -1,9 +1,27 @@
+require("dotenv").config()
 const express = require("express")
 const app = express()
 const cors = require("cors")
+const mongoose = require("mongoose")
+const db = mongoose.connection;
+const collection = db.collection('biko');
 
-app.get("/", (req, res) => {
-    res.send("Hello from ecomm server")
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect(process.env.MONGO_URI,  { useNewUrlParser: true }).then(() => {
+    app.listen(process.env.PORT, () => console.log(`http://localhost:${process.env.PORT}`))
 })
 
-app.listen(3000, () => console.log("http://localhost:3000"))
+const MyCollectionSchema = new mongoose.Schema({author: String} );
+
+const MyCollection = mongoose.model('biko', MyCollectionSchema);
+
+app.get("/", (req, res) => {
+    res.json({json:"Hello from ecomm server"})
+    const name = "Steven Pierre"
+    MyCollection.findOne({author: name}).then(data => console.log(data))
+})
+
+
+
