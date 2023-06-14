ded0f0dfbe7b5e247d2dd0e9b1d89d343a337173
StevenMODIMO <stevepetes26@gmail.com>
2023-06-14 21:18:57 +0300

    Initial Commit

--- client/src/pages/Business.jsx
+++ client/src/pages/Business.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useAuth } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
 import { BiArrowBack } from "react-icons/bi";
@@ -12,6 +12,9 @@ export default function Business() {
   const [loading, setLoading] = useState(false)
   const { user } = useAuth();
   const navigate = useNavigate();
+  const [check, setCheck] = useState(false)
+  const [city, setCity] = useState("")
+  const [country, setCountry] = useState("")
 
   const handleSubmission = async (e) => {
     e.preventDefault();
@@ -45,12 +48,34 @@ export default function Business() {
 
 	setLoading(false)
   };
+
+  useEffect(() => {
+    const getLocation = async() => {
+      const response = await fetch("https://ipinfo.io/json?token=b068a85af3c0f9")
+      const json = await response.json()
+
+      if(response.ok) {
+        setCity(json.city)
+        setCountry(json.country)
+      }
+    }
+    getLocation()
+  }, [])
+
+  useEffect(() => {
+    if (check) {
+      setAddress(city+","+country);
+    } else {
+      setAddress("")
+    }
+  }, [check, city, country]);
+
   return (
     <div className="flex flex-col gap-10 shadow-2xl mt-10 mx-2 border-t-2 border-black sm:mx-36 lg:mx-96 h-96">
 		{error && <Message text={error} />}
       <header className="flex flex-col items-center mt-10">
         <div
-          className="bg-yellow-400 p-1  rounded text-black w-fit flex gap-3"
+          className="bg-yellow-400 p-1 rounded text-black w-fit flex gap-3"
           onClick={() => navigate("/")}
         >
           <BiArrowBack className="mt-1" />
@@ -61,7 +86,7 @@ export default function Business() {
       <form
         onSubmit={handleSubmission}
         onFocus={() => setError(null)}
-        className="flex flex-col items-center justify-center gap-3  p-1 m-1 rounded"
+        className="flex flex-col items-center justify-center gap-3 p-1 m-1 rounded"
       >
         <input
           type="text"
@@ -77,6 +102,10 @@ export default function Business() {
           className="border-2 border-yellow-400 outline-none rounded p-1 text-lg sm:w-80"
           placeholder="address"
         />
+        <label className="text-sm flex gap-1 ml-0 sm:text-lg">
+        <input type="checkbox" checked={check} onChange={(e) => setCheck(e.target.checked)} />
+        <span className="bg-yellow-200 px-1">Use current location ?</span>
+        </label>
 		{loading ? <Loader /> :
         <button className="bg-yellow-500 p-1 rounded">Register Business</button>}
       </form>
