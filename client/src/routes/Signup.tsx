fadd61b0aeb52a2d0787913569bd9b5e889e8360
StevenMODIMO <stevepetes26@gmail.com>
2025-10-08 12:48:44 +0300

    v2: mern-ecommerce-store

--- client/src/routes/Signup.tsx
+++ client/src/routes/Signup.tsx
@@ -1,3 +1,61 @@
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardFooter,
+  CardContent,
+} from "@/components/ui/card";
+
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+
 export default function Signup() {
-  return <div>Get started</div>;
+  return (
+    <div>
+      <Card className="border-none shadow-xs sm:w-[60%] sm:mx-auto lg:w-[30%]">
+        <CardHeader>
+          <CardTitle>Sign Up</CardTitle>
+          <CardDescription>Create a new account</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form className="space-y-4">
+            <div>
+              <Label htmlFor="username">Username</Label>
+              <Input
+                type="text"
+                id="username"
+                placeholder="Enter your username"
+                className="mt-1"
+              />
+            </div>
+            <div>
+              <Label htmlFor="email">Email</Label>
+              <Input
+                type="email"
+                id="email"
+                placeholder="Enter your email"
+                className="mt-1"
+              />
+            </div>
+            <div>
+              <Label htmlFor="password">Password</Label>
+              <Input
+                type="password"
+                id="password"
+                placeholder="Enter your password"
+                className="mt-1"
+              />
+            </div>
+            <CardFooter>
+              <Button type="submit" className="w-full">
+                Sign Up
+              </Button>
+            </CardFooter>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
