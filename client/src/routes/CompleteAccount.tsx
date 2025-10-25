87b9e7604483a10e32c7a5ddb6f092075be08e21
StevenMODIMO <stevepetes26@gmail.com>
2025-10-25 11:23:21 +0300

    v2: mern-store-app

--- client/src/routes/CompleteAccount.tsx
+++ client/src/routes/CompleteAccount.tsx
@@ -1,7 +1,15 @@
 import { useEffect } from "react";
 import { useAuthContext } from "@/hooks/useAuthContext";
 import { useNavigate } from "react-router";
-import { Navigate } from "react-router-dom";
+
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardFooter,
+  CardContent,
+} from "@/components/ui/card";
 
 export default function CompleteAccount() {
   const { state } = useAuthContext();
@@ -13,5 +21,21 @@ export default function CompleteAccount() {
       navigate("/dashboard");
     }
   }, [state.user]);
-  return <div>CompleteAccount</div>;
+  return (
+    <div>
+      <Card className="md:w-[60%] md:mx-auto lg:w-[50%]">
+        <CardHeader>
+          <CardTitle className="text-[#737373] text-xl sm:text-center md:text-start lg:text-2xl">
+            Please take a moment to set up your account.
+          </CardTitle>
+          <CardDescription className="sm:text-center md:text-start">
+            You have two options to set up your account: you can choose to be a{" "}
+            <strong>Buyer</strong> or a <strong>Seller</strong>. Please select
+            the role that best fits your needs to proceed.
+          </CardDescription>
+        </CardHeader>
+      </Card>
+    </div>
+  );
 }
+F
